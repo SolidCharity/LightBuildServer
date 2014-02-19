b23823ecc0443b3cc94fd8b31daabc217bfc782e
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2014-02-19 16:50:25 +0100

    small fixes for static IP address

--- lxc/ubuntu-precise/clone-centos6.sh
+++ lxc/ubuntu-precise/clone-centos6.sh
@@ -18,6 +18,14 @@ sed -i "s/HOSTNAME=.*/HOSTNAME=$name/g" $rootfs_path/etc/sysconfig/network
 sed -i "s/^#lxc\.network\.ipv4.*/lxc.network.ipv4=10.0.3.$cid/g" $config_path/config
 sed -i 's/^BOOTPROTO=dhcp/#BOOTPROTO=dhcp/g' ${rootfs_path}/etc/sysconfig/network-scripts/ifcfg-eth0
 sed -i 's/^#BOOTPROTO=none/BOOTPROTO=none/g' ${rootfs_path}/etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i "s/^#IPADDR/IPADDR/g" ${rootfs_path}/etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i 's/^#GATEWAY/GATEWAY/g' ${rootfs_path}/etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i 's/^#NETMASK/NETMASK/g' ${rootfs_path}/etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i 's/^#NETWORK/NETWORK/g' ${rootfs_path}/etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i "s/^IPADDR.*/IPADDR=10.0.3.$cid/g" ${rootfs_path}/etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i 's/^GATEWAY.*/GATEWAY=10.0.3.1/g' ${rootfs_path}/etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i 's/^NETMASK.*/NETMASK=255.255.255.0/g' ${rootfs_path}/etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i 's/^NETWORK.*/NETWORK=10.0.3.0/g' ${rootfs_path}/etc/sysconfig/network-scripts/ifcfg-eth0
 sed -i 's/^DHCP_HOSTNAME/#DHCP_HOSTNAME/g' ${rootfs_path}/etc/sysconfig/network-scripts/ifcfg-eth0
 ./tunnelssh.sh $name $cid
 cp $rootfs_path/usr/share/zoneinfo/Europe/Berlin $rootfs_path/etc/localtime
