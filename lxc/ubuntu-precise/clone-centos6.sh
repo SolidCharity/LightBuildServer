adcd6b7a1baec4e2e2f65f617e65d259d94f7420
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2014-02-19 12:04:38 +0100

    small fix for cloning centos

--- lxc/ubuntu-precise/clone-centos6.sh
+++ lxc/ubuntu-precise/clone-centos6.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-name=70-centos6-lbs
+
+if [ -z $2 ]
+then
+  echo "please call $0 <name of new container> <cid>"
+  echo "   eg. $0 50-centos6-mymachine 50"
+  exit 1
+fi
+name=$1
+cid=$2
+
 lxc-clone -o centos6-master -n $name
 sed -i "s/centos6-master/$name/g" /var/lib/lxc/$name/config
+./tunnelssh.sh $name $cid
