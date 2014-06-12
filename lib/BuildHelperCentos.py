62a736826c505a9f85182c10752c0c528a126503
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2014-06-12 13:32:17 +0200

    for CentOS5: we need make, but it is not part of buildrequires (eg. autoconf)

--- lib/BuildHelperCentos.py
+++ lib/BuildHelperCentos.py
@@ -59,6 +59,7 @@ class BuildHelperCentos(BuildHelper):
     # CentOS5: /root/rpmbuild should point to /usr/src/redhat
     if self.dist == "centos" and self.container.release == "5":
       self.run("mkdir -p /usr/src/redhat; ln -s /usr/src/redhat rpmbuild")
+      self.run("yum -y install make")
     self.run("mkdir -p rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}");
 
   def GetSpecFilename(self):
