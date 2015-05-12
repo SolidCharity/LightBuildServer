4cb15362ccab2ee67365f88bd601a4650757e5be
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2015-05-12 04:53:47 +0000

    RemoteContainer is now the base class

---
+++ lib/RemoteContainer.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+"""Interface class for Container Management"""
+
+# Copyright (c) 2014-2015 Timotheus Pokorra
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+# USA
+#
+
+import sys
+import os
+import time
+import socket
+from Logger import Logger
+from Shell import Shell
+
+class RemoteContainer:
+  def __init__(self, containername, configBuildMachine, logger):
+    self.name = containername
+
+    self.port=22
+    if "port" in configBuildMachine:
+      self.port=str(configBuildMachine['port'])
+    self.cid=10
+    if "cid" in configBuildMachine:
+      self.cid=configBuildMachine['cid']
+
+    self.containerIP=socket.gethostbyname(self.name)
+    self.containerPort=str(2000+int(self.cid))
+
+    # we just test if the host server for the build container is actually hosting the LBS application as well
+    lbsipaddress=socket.gethostbyname(socket.gethostname()).split('.')
+    buildserveraddress=socket.gethostbyname(containername).split('.')
+
+    if buildserveraddress[3] == "1":
+      buildserveraddress.pop()
+      lbsipaddress.pop()
+      if '.'.join(buildserveraddress) == '.'.join(lbsipaddress):
+        self.containerIP='.'.join(buildserveraddress) + "." + str(self.cid)
+        self.containerPort="22"
+
+    self.logger = logger
+    self.shell = Shell(logger)
+    # we are reusing the slots, for caches etc
+    self.slot = containername
+    self.distro = ""
+    self.release = ""
+    self.arch = ""
+    self.staticIP = ""
+
+  def executeOnHost(self, command):
+    # not implemented here
+    return False
+
+  def createmachine(self, distro, release, arch, staticIP):
+    # not implemented here
+    return False
+
+  def startmachine(self):
+    # not implemented here
+    return False
+
+  def executeInContainer(self, command):
+    """Execute a command in a container via SSH"""
+    # not implemented here
+    return False
+
+  def destroy(self):
+    # not implemented here
+    return False
+
+  def stop(self):
+    # not implemented here
+    return False
+
+  def copytree(self, src, dest):
+    # not implemented here
+    return False
+
+  def rsyncContainerGet(self, path, dest = None):
+    # not implemented here
+    return False
+
+  def rsyncHostPut(self, src, dest = None):
+    # not implemented here
+    return False
+
+  def rsyncHostGet(self, path, dest = None):
+    # not implemented here
+    return False
+
+  def installmount(self, localpath, hostpath = None):
+    # not implemented here
+    return False
