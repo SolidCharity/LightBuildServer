8a18716784085eaf0e068ed9b4a3744895e6a1d1
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2015-08-25 14:16:27 +0200

    add a script for installing the source into the test system

---
+++ makeinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# these lines will install the current source on top of a installed package
+
+alias cp=cp
+src=/root/LightBuildServer
+prod=/usr/share/lightbuildserver
+
+if [ ! -d $prod ]
+then
+  echo "please first install the lightbuildserver rpm: dnf install lightbuildserver"
+  exit 1
+fi
+
+cp -fR $src/web/* $prod/web
+cp -fR $src/lib/* $prod/lib
+systemctl restart uwsgi
