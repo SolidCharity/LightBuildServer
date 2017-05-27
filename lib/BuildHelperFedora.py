2d915777e98a12b52760644561d84d7af0426a54
Timotheus Pokorra <tp@tbits.net>
2017-05-27 09:09:42 +0200

    update Fedora versions. no updates-testing for Fedora 25

--- lib/BuildHelperFedora.py
+++ lib/BuildHelperFedora.py
@@ -30,9 +30,9 @@ class BuildHelperFedora(BuildHelperCentos):
     BuildHelperCentos.__init__(self, container, username, projectname, packagename, branchname)
     self.dist='fedora'
     self.rhel = 0
-    self.rawhide = 26
+    self.rawhide = 27
     # this is the latest release. rawhide might be already 25, 23 latest release, 24 in testing
-    self.latestrelease = 24
+    self.latestrelease = 25
     if self.release == "rawhide":
      self.release = self.rawhide
     self.release = str(self.release)
