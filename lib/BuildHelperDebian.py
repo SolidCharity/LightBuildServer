316ef05ae2359e4213a4743892aff8735f062728
Timotheus Pokorra <tp@tbits.net>
2018-07-11 14:56:15 +0200

    Ubuntu 18: need iproute2 as required package

--- lib/BuildHelperDebian.py
+++ lib/BuildHelperDebian.py
@@ -42,7 +42,7 @@ class BuildHelperDebian(BuildHelper):
       return False
     if not self.run("DEBIAN_FRONTEND=noninteractive apt-get -y upgrade"):
       return False
-    if not self.run("apt-get -y install build-essential ca-certificates iptables curl apt-transport-https dpkg-sig reprepro wget rsync devscripts equivs"):
+    if not self.run("apt-get -y install build-essential ca-certificates iptables curl apt-transport-https dpkg-sig reprepro wget rsync devscripts equivs iproute2"):
       #apt-utils
       return False
     # make sure we have a fully qualified hostname
