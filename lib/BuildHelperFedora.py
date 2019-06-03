310b4bcc9a93201fc058f36bca2bf1f3d28d975a
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2019-06-03 07:49:59 +0200

    upgrade Fedora to latest release F30

--- lib/BuildHelperFedora.py
+++ lib/BuildHelperFedora.py
@@ -30,9 +30,9 @@ class BuildHelperFedora(BuildHelperCentos):
     BuildHelperCentos.__init__(self, container, username, projectname, packagename, branchname)
     self.dist='fedora'
     self.rhel = 0
-    self.rawhide = 30
+    self.rawhide = 31
     # this is the latest release. rawhide might be already 25, 23 latest release, 24 in testing
-    self.latestrelease = 29
+    self.latestrelease = 30
     if self.release == "rawhide":
      self.release = self.rawhide
     self.release = str(self.release)
