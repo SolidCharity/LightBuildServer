a976e8a5b870175c71491b2da39692b0c0c0e346
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-10-25 17:50:08 +0200

    improve admin view for projects

--- projects/admin.py
+++ projects/admin.py
@@ -3,9 +3,24 @@ from django.contrib import admin
 from .models import Project, Package, Distro, Branch
 
 
-admin.site.register([
-    Project,
-    Package,
-    Distro,
-    Branch,
-])
+class DistroAdminInline(admin.TabularInline):
+    model = Distro
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['user', 'name', 'visible']
+
+    class Meta:
+        None
+
+
+class PackageAdmin(admin.ModelAdmin):
+    list_display = ['name', 'project']
+    inlines = (DistroAdminInline, )
+
+    class Meta:
+        None
+
+
+admin.site.register(Package, PackageAdmin)
+admin.site.register(Project, ProjectAdmin)
--- projects/models.py
+++ projects/models.py
@@ -9,17 +9,17 @@ class Project(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     git_url = models.CharField(max_length=250)
     git_branch = models.CharField(max_length=250)
-    use_lxc = models.BooleanField()
     public_key = models.TextField(null=True, blank=True)  # generate keys autom. at project setup
     private_key = models.TextField(null=True, blank=True)
     machine = models.ForeignKey(Machine, on_delete=models.SET_NULL, null=True, blank=True)
     copr_user_name = models.CharField(max_length=250, null=True, blank=True)    # solve copr-stuff later
     copr_project_name = models.CharField(max_length=250, null=True, blank=True)
+    use_lxc = models.BooleanField()
     use_docker = models.BooleanField()
     visible = models.BooleanField()
 
     def __str__(self):
-        return self.name
+        return f"{self.user}::{self.name}"
     
     def get_buildtargets(self):
         return sorted(set([distro for package in self.package_set.all() for distro in package.distro_set.all()]))
