9371757c58c1c56e79587ad3fc2ae487124bc988
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-10-26 09:07:44 +0200

    prevent non public projects and packages from being visible

--- projects/views.py
+++ projects/views.py
@@ -24,21 +24,37 @@ class ProjectView(generic.DetailView):
     context_object_name = "project"
 
     def get_object(self):
-        return Project.objects.get(
+        project = Project.objects.get(
             user__exact=User.objects.get(username__exact=self.kwargs["user"]),
             name__exact=self.kwargs["project"],
             git_branch__exact=self.kwargs["branch"]
         )
+
+        # only public projects, or own project, or I am staff
+        if project.visible == False:
+            if not self.request.user.is_staff:
+                if self.request.user != project.user:
+                    raise Exception("you do not have permission for this project")
+
+        return project
 
 class PackageView(generic.DetailView):
     template_name = "projects/package.html"
     context_object_name = "package"
 
     def get_object(self):
-        return Package.objects.get(
+        package = Package.objects.get(
             project__exact=Project.objects.get(
                 user__exact=User.objects.get(username__exact=self.kwargs["user"]),
                 name__exact=self.kwargs["project"]
             ),
             name__exact=self.kwargs["package"]
         )
+
+        # only public projects, or own project, or I am staff
+        if package.project.visible == False:
+            if not self.request.user.is_staff:
+                if self.request.user != package.project.user:
+                    raise Exception("you do not have permission for this project")
+
+        return package
