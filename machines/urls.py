840d1a97580a8be1bb69995d936ff9ed55004e93
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-11-08 11:33:44 +0100

    fix url for resetting machine

--- machines/urls.py
+++ machines/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'machines'
 urlpatterns = [
     path('', views.monitor, name='monitor'),
-    path('machines/reset/<str:machine>', views.reset, name='reset'),
+    path('machines/reset/<str:machine_name>', views.reset, name='reset'),
 ]
